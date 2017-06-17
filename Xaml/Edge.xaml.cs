643551f12264050355f3c1b96a3514aff56c4eb0
Dan Kvelstad <dan.kvelstad@gmail.com>
2017-06-17 22:15:27 +0200

    compensated for rounded corner

--- Xaml/Edge.xaml.cs
+++ Xaml/Edge.xaml.cs
@@ -29,34 +29,75 @@ namespace Grapher
 
             if(FromState.Center.X < ToState.Center.X)
             {
-                TheLine.X1 = FromState.Center.X + FromState.Width/2;
-                TheLine.X2 =   ToState.Center.X -   ToState.Width/2;
+                if (FromState.Center.Y < ToState.Center.Y)
+                {
+                    TheLine.X1 = FromState.Center.X + FromState.Width /2 - FromState.StateBorder.CornerRadius.BottomRight*Math.Cos(Math.PI/4);
+                    TheLine.Y1 = FromState.Center.Y + FromState.Height/2 - FromState.StateBorder.CornerRadius.BottomRight*Math.Sin(Math.PI/4);
+                    TheLine.X2 =   ToState.Center.X -   ToState.Width /2 +   ToState.StateBorder.CornerRadius.TopLeft    *Math.Cos(Math.PI/4);
+                    TheLine.Y2 =   ToState.Center.Y -   ToState.Height/2 +   ToState.StateBorder.CornerRadius.TopLeft    *Math.Sin(Math.PI/4);
+                }
+                else if (FromState.Center.Y > ToState.Center.Y)
+                {
+                    TheLine.X1 = FromState.Center.X + FromState.Width /2 - FromState.StateBorder.CornerRadius.TopRight  *Math.Cos(Math.PI/4);
+                    TheLine.Y1 = FromState.Center.Y - FromState.Height/2 + FromState.StateBorder.CornerRadius.TopRight  *Math.Sin(Math.PI/4);
+                    TheLine.X2 =   ToState.Center.X -   ToState.Width /2 +   ToState.StateBorder.CornerRadius.BottomLeft*Math.Cos(Math.PI/4);
+                    TheLine.Y2 =   ToState.Center.Y +   ToState.Height/2 -   ToState.StateBorder.CornerRadius.BottomLeft*Math.Sin(Math.PI/4);
+                }
+                else
+                {
+                    TheLine.X1 = FromState.Center.X + FromState.Width/2;
+                    TheLine.Y1 = FromState.Center.Y;
+                    TheLine.X2 =   ToState.Center.X -   ToState.Width/2;
+                    TheLine.Y2 =   ToState.Center.Y;
+                }
             }
             else if(FromState.Center.X > ToState.Center.X)
             {
-                TheLine.X1 = FromState.Center.X - FromState.Width/2;
-                TheLine.X2 =   ToState.Center.X +   ToState.Width/2;
+                if (FromState.Center.Y < ToState.Center.Y)
+                {
+                    TheLine.X1 = FromState.Center.X - FromState.Width /2 + FromState.StateBorder.CornerRadius.BottomLeft*Math.Cos(Math.PI/4);
+                    TheLine.Y1 = FromState.Center.Y + FromState.Height/2 - FromState.StateBorder.CornerRadius.BottomLeft*Math.Sin(Math.PI/4);
+                    TheLine.X2 =   ToState.Center.X +   ToState.Width /2 -   ToState.StateBorder.CornerRadius.TopRight  *Math.Cos(Math.PI/4);
+                    TheLine.Y2 =   ToState.Center.Y -   ToState.Height/2 +   ToState.StateBorder.CornerRadius.TopRight  *Math.Sin(Math.PI/4);
+                }
+                else if (FromState.Center.Y > ToState.Center.Y)
+                {
+                    TheLine.X1 = FromState.Center.X - FromState.Width /2 + FromState.StateBorder.CornerRadius.TopLeft    *Math.Cos(Math.PI/4);
+                    TheLine.Y1 = FromState.Center.Y - FromState.Height/2 + FromState.StateBorder.CornerRadius.TopLeft    *Math.Sin(Math.PI/4);
+                    TheLine.X2 =   ToState.Center.X + ToState.Width   /2 -   ToState.StateBorder.CornerRadius.BottomRight*Math.Cos(Math.PI/4);
+                    TheLine.Y2 =   ToState.Center.Y +   ToState.Height/2 -   ToState.StateBorder.CornerRadius.BottomRight*Math.Sin(Math.PI/4);
+                }
+                else
+                {
+                    TheLine.X1 = FromState.Center.X - FromState.Width / 2;
+                    TheLine.Y1 = FromState.Center.Y;
+                    TheLine.X2 =   ToState.Center.X + ToState.Width / 2;
+                    TheLine.Y2 =   ToState.Center.Y;
+                }
             }
             else
             {
-                TheLine.X1 = FromState.Center.X;
-                TheLine.X2 =   ToState.Center.X;
-            }
-
-            if (FromState.Center.Y < ToState.Center.Y)
-            {
-                TheLine.Y1 = FromState.Center.Y + FromState.Height/2;
-                TheLine.Y2 =   ToState.Center.Y -   ToState.Height/2;
-            }
-            else if (FromState.Center.Y > ToState.Center.Y)
-            {
-                TheLine.Y1 = FromState.Center.Y - FromState.Height/2;
-                TheLine.Y2 =   ToState.Center.Y +   ToState.Height/2;
-            }
-            else
-            {
-                TheLine.Y1 = FromState.Center.Y;
-                TheLine.Y2 =   ToState.Center.Y;
+                if (FromState.Center.Y < ToState.Center.Y)
+                {
+                    TheLine.X1 = FromState.Center.X;
+                    TheLine.X2 =   ToState.Center.X;
+                    TheLine.Y1 = FromState.Center.Y + FromState.Height/2;
+                    TheLine.Y2 =   ToState.Center.Y -   ToState.Height/2;
+                }
+                else if (FromState.Center.Y > ToState.Center.Y)
+                {
+                    TheLine.X1 = FromState.Center.X;
+                    TheLine.X2 =   ToState.Center.X;
+                    TheLine.Y1 = FromState.Center.Y - FromState.Height/2;
+                    TheLine.Y2 =   ToState.Center.Y +   ToState.Height/2;
+                }
+                else
+                {   // this is actually an error state, they overlap
+                    TheLine.X1 = FromState.Center.X;
+                    TheLine.X2 =   ToState.Center.X;
+                    TheLine.Y1 = FromState.Center.Y;
+                    TheLine.Y2 =   ToState.Center.Y;
+                }
             }
 
         }
