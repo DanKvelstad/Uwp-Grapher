5b78d7f5ae0aa4da6e638ca56ba87a2e49d4e9cd
Dan Kvelstad <dan.kvelstad@itk-engineering.de>
2018-01-24 08:31:52 +0100

    Still broken, but view is seperated into files

---
+++ Views/DataTemplates/GraphElementSelector.cs
@@ -0,0 +1,40 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Grapher.Views.DataTemplates
+{
+    public class GraphElementSelector : DataTemplateSelector
+    {
+
+        public DataTemplate NodeTemplate
+        {
+            get;
+            set;
+        }
+
+        public DataTemplate EdgeTemplate
+        {
+            get;
+            set;
+        }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+
+            if (item is ViewModels.NodeViewModel)
+            {
+                return NodeTemplate;
+            }
+            else if (item is ViewModels.EdgeViewModel)
+            {
+                return EdgeTemplate;
+            }
+            else
+            {
+                return base.SelectTemplateCore(item, container);
+            }
+
+        }
+    }
+
+}
